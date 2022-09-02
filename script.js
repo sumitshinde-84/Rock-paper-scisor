f1828f827edd6dc160db6eea54c830a17d1438be
sumitshinde-84 <shindesumit217@gmail.com>
2022-09-02 18:26:45 +0530

    all console result added on document

--- script.js
+++ script.js
@@ -77,10 +77,11 @@ for(i=1;i<=5;i++){
     console.log(`Round ${i} \n`)
     document.write(`Round ${i} `)
     document.write("<br>")
+    document.write("<br>")
 
     // input 
     
-     plr =  prompt("Enter your choice (Rock- R, paper- P , Scisor- S");
+     plr =  prompt("Enter your choice (for Rock- R, paper- P , Scisor- S");
      
     
 
@@ -143,19 +144,19 @@ else{
 if (plr=='R'){
     document.write("you choosed :ROCK")
     document.write("<br>")
-    document.write("<br>")
+    
 }
 
 else if (plr=='P'){
     document.write("you choosed :PAPER")
     document.write("<br>")
-    document.write("<br>")
+   
 }
 
 else{
     document.write("you choosed :SCISOR")
     document.write("<br>")
-    document.write("<br>")
+   
 }
  
 
@@ -165,8 +166,17 @@ else{
 
     // current round point show
 
-    console.log(`your point:${point}`);
-    console.log(`computer point:${cp}`);
+    console.log(`your point: ${point}`);
+    document.write(`Your point: ${p}`)
+    document.write("<br>")
+   
+    console.log(`computer point: ${cp}`);
+    document.write(`computer point:  ${cp}`)
+    document.write("<br>");
+    document.write("<br>");
+    document.write("<br>");
+    document.write("<hr>");
+
 
     
     
@@ -176,17 +186,25 @@ else{
 
 console.log("\n")
 console.log(` Your Total point= ${p}`);
+document.write(`<center><strong><h3>Your Total point:  ${p}</h3></strong>`)
+
 console.log(`Total Computer point= ${c}`);
+document.write(`<center><strong><h3>Computer Total point:  ${c}</h3></strong>`)
+document.write("<br>");
+document.write("<br>");
 
 
 // final result
 
 if(p >c){
     console.log("CONGRATULATIONS YOU WIN " );
+    document.write(`<center><h2>CONGRATULATIONS YOU WIN </h2>`);
 } else if(c==p){
     console.log("TIE");
+    document.write(`<center><h2>TIE....</h2>`);
 }
 
 else{
     console.log("SHIT.. YOU LOSE");
+    document.write(`<center><h2>SHIT.. YOU LOSE</h2>`);
 }
